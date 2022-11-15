241784b7b116ba549bbc0fcaf3f7772103e58eeb
박건웅 <pkw132@naver.com>
2022-11-15 13:29:04 +0900

    solve

---
+++ Input output and normal operation.py
@@ -0,0 +1,16 @@
+#백준 2557번 / Hello World!를 화면에 출력하는 문제 (예제 출력과 똑같이 출력해야 합니다.)
+
+print("Hello World!")
+
+#백준 1000번 (0 < A, B < 10) / 두 수를 입력받고 합을 출력하는 문제
+
+A,B=map(int,input().split())
+
+print(A+B)
+
+#백준 1001번 (0 < A, B < 10) / 두 수를 입력받고 뺄셈을 한 결과를 출력하는 문제
+
+A,B=map(input().split())
+
+print(A)
+print(B)
