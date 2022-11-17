c5908b98c0416e77d2e34210b2304c79b4b41ffe
박건웅 <pkw132@naver.com>
2022-11-17 16:00:00 +0900

    solved 2753

--- python/조건문/num9498.py
+++ python/조건문/num9498.py
@@ -8,14 +8,14 @@ num=int(input())
 if(90<=num<=100):
     print("A")
 
-if(80<=num<=89):
-    print("A")
+elif(80<=num<=89):
+    print("B")
 
-if(70<=num<=79):
-    print("A")
+elif(70<=num<=79):
+    print("C")
 
-if(60<=num<=69):
-    print("A")
+elif(60<=num<=69):
+    print("D")
 
 else:
     print("F")
