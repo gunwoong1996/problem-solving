84c71abbfce3d1b28adc3f32d147ea80f9a173be
gunwoong1996 <pkw132@naver.com>
2025-02-01 23:37:28 +0900

    Update do.py

--- 예제/do.py
+++ 예제/do.py
@@ -1,3 +1,10 @@
-num = "1234567"
-num_str = str(num)  # 숫자를 문자열로 변환
-print(num_str[1:7])  # 뒤에서부터 7자리 출력
+word = " HIHIHIHHIIH"
+
+print(word.lower())
+print(len(word))
+
+print(word.replace("HI","bye"))
+
+print(word.find("NO"))
+
+print(word.count("H"))
