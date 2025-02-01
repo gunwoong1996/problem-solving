c9f8f50e5f97d224386e9d0245bf19bcace156bc
gunwoong1996 <pkw132@naver.com>
2025-02-01 23:31:52 +0900

    Update do.py

--- 예제/do.py
+++ 예제/do.py
@@ -1,10 +1,3 @@
-from random import *
-
-num=(randint(1,28))
-
-print(num)
-
-if(num <= 25):    
-    print("스터디 준비기간입니다")
-else:
-    print("오프라인 스터디 모임 날짜는 매월",num ,"일로 선정되었습니다.")
+num = "1234567"
+num_str = str(num)  # 숫자를 문자열로 변환
+print(num_str[1:7])  # 뒤에서부터 7자리 출력
