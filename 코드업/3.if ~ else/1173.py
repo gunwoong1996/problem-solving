3c2fd38cd0c6c6e49f73dbaa51d72e655985cd7f
gunwoong1996 <pkw132@naver.com>
2025-11-25 00:28:43 +0900

    Update 1173.py

--- 코드업/3.if ~ else/1173.py
+++ 코드업/3.if ~ else/1173.py
@@ -1,10 +1,19 @@
 a,b, = map(int,input().split())
 
-time = b-30
+time = b-30 
 
 if time<0:
-    a-1
-    print(a,time)
+    a-=1
+    #print(a,(time+60))
+    if a<0:
+        print(a+24,(time+60))
+    else:
+        print(a,(time+60))
+
+elif time == 0:
+    print(a,0)
+
+
 
 else:
     print(a,time)
