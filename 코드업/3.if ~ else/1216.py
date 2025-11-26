5d94ff232631ab4e04693c747c7d4c4167150199
gunwoong1996 <pkw132@naver.com>
2025-11-26 23:47:14 +0900

    .

---
+++ 코드업/3.if ~ else/1216.py
@@ -0,0 +1,11 @@
+a,b,c = map(int,input().split())
+
+
+if b - c > a:
+    print("advertise")
+
+elif b - c < a:
+    print("do not advertise")
+
+else:
+    print("does not matter")
---
+++ 코드업/3.if ~ else/1218.py
@@ -0,0 +1,17 @@
+a,b,c = map(int,input().split()) # a<=b<=c
+
+
+if a == b == c:
+    print("정삼각형")
+
+elif a>b+c or b>a+c or c>a+b or a == b+c or b == a+c or c == b+a:
+    print("삼각형아님") 
+
+elif a == b or a == c or b ==c :
+    print("이등변삼각형")
+
+elif ((a**2) + (b**2)) == (c**2):
+    print("직각삼각형")
+
+else:
+    print("삼각형")
