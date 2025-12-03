578730e42f1d54bc340ca8409e2933c5178c7421
박건웅 <pkw132@naver.com>
2025-12-03 17:12:41 +0900

    .

--- 코드업/4-1.단순 반복문/5079.py
+++ 코드업/4-1.단순 반복문/5079.py
@@ -1,2 +1,18 @@
-s = input()
-print(s.swapcase()) #대소문자 변환
+v = int(input())
+
+result = input().strip()     # 공백 없이도 처리 가능
+ac = 0
+bc = 0
+
+for ch in result:
+    if ch == 'A':
+        ac += 1
+    elif ch == 'B':
+        bc += 1
+
+if ac > bc:
+    print("A")
+elif ac < bc:
+    print("B")
+else:
+    print("Tie")
