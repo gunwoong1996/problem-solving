ccdd24ff30b852cd496c3ab442658819062f63d0
박건웅 <pkw132@naver.com>
2025-12-10 11:58:51 +0900

    .

--- 코드업/4-2 중첩 반복문/1356.py
+++ 코드업/4-2 중첩 반복문/1356.py
@@ -1,6 +1,8 @@
 n = int(input())  # 삼각형의 길이
 
 for i in range(1,n+1):
-    print("*"*n)
-    if i < n:
-        print("*",sep=" " * i)
+    if i == n or i == 1:
+        print("*"*n)
+    else: 
+        print("*" + " " *(n-2)+"*")
+   
