45788d3215b09f7280dd316939cb79fc5e917cda
박건웅 <pkw132@naver.com>
2025-12-16 17:10:23 +0900

    12.16

---
+++ 코드업/4-2 중첩 반복문/1382.py
@@ -0,0 +1,10 @@
+
+for i in range(1,10):
+
+    for j in range(2,6):
+         print(f"{j} x {i} = {i*j:2}", end="") #:2 = 너비2칸 = 구구단 우측 정렬
+         
+         print("  ",end = " ")
+
+    print()
+
