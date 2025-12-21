37642f67a0a61504810bcd832ab5e1387c7d4521
gunwoong1996 <pkw132@naver.com>
2025-12-22 00:26:52 +0900

    Update practice.py

--- 예제/practice.py
+++ 예제/practice.py
@@ -1,10 +1,4 @@
-name = "연탄이"
-
-animal = "강아지"
-
-age = 4
-
-print(f"우리집 {animal}의 이름은 {name}이에요")
+station = ["사당","신도림","인천공항"]
 
 
-print(f"{name}는 {age}살이며 {animal}을 좋아해요")
+print(f"{station[0]} 행 열차가 들어오고 있습니다.")
