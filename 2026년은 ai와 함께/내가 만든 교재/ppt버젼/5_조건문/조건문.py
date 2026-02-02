af129574277f01106f4b4246e2d3f5acea2f79f2
박건웅 <pkw132@naver.com>
2026-02-02 16:44:11 +0900

    .

---
+++ 2026년은 ai와 함께/내가 만든 교재/ppt버젼/5_조건문/조건문.py
@@ -0,0 +1,20 @@
+# user = input()
+# if user:
+#     print("로그인")
+# else:
+#     print("비로그인")
+
+
+# data = {"a": 1}
+# if "a" in data:
+#     print(data["a"])
+
+article = {
+    "title": "뉴스 제목",
+    "view": 1234
+}
+
+if "title" in article:
+    print(article["view"])
+else:
+    print("이메일 없음")
