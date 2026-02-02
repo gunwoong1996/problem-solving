bacc1f1ddc1ca99cdc9c2aaec53d9693c8a2da2a
박건웅 <pkw132@naver.com>
2026-02-02 09:28:57 +0900

    .

---
+++ 2026년은 ai와 함께/내가 만든 교재/ppt버젼/3_입출력/입출력.py
@@ -0,0 +1,27 @@
+# a,b = map(int,input().split())
+
+# print(a,b)
+
+
+#============================================
+
+# arr = list(map(int,input().split()))
+
+# print(arr)
+
+#============================================
+print("hello")
+print("world")
+print("a","b","c")
+print(1, 2, 3, sep='/')
+
+print(1, end=' ')
+print(2)
+
+name = "kim"
+
+age = 20
+
+print(f"{name} {age}")
+
+print("%d %d" % (3, 5))
