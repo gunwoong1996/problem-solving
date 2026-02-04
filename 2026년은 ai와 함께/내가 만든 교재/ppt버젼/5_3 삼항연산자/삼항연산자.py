1e9cec935cea5434fe75b2e0e00ec8ae7f9ffac7
박건웅 <pkw132@naver.com>
2026-02-04 17:17:49 +0900

    .

---
+++ 2026년은 ai와 함께/내가 만든 교재/ppt버젼/5_3 삼항연산자/삼항연산자.py
@@ -0,0 +1,12 @@
+# login = True
+# admin = False
+# if login or admin:
+#     print("관리자")
+
+# pw = input()
+# if pw != "" and len(pw) >= 8:
+#     print("사용 가능")
+
+arr = []
+if not arr:
+    print("비어 있음")
---
+++ 2026년은 ai와 함께/내가 만든 교재/ppt버젼/6_반복문/반복문.py
@@ -0,0 +1,44 @@
+"""
+for i in range(3):
+    print(i)
+
+arr = [10, 20, 30]
+for x in arr:
+    print(x)
+
+s = "abc"
+for ch in s:
+    print(ch)
+
+for i in range(10):
+    if i == 5:
+        break
+    print(i)
+"""
+
+for i in range(5):
+    if i == 2:
+        continue
+    print(i)
+
+# while True:
+#     x = input()
+#     if x == "q":
+#         break
+
+
+for i in range(2):
+    for j in range(3):
+        print(i, j)
+
+#====================================
+
+for i in range(3):
+    if i == 1:
+        pass
+    print(i)
+
+for i in range(3):
+    if i == 1:
+        continue
+    print(i)    
